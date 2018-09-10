dc6280d82ecfa0892737321fe35c4a295c85f095
Donald Hsu <donald.hsu@moxa.com>
2018-09-10 13:08:17 +0800

    0.1.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(*paths):
 
 setuptools.setup(
     name="sanjip",
-    version="0.1.0",
+    version="0.1.1",
     author="Sanji Team",
     author_email="sanji@moxa.com",
     description="Sanji Bundle IP Module",
